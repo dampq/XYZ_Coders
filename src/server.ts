7f1dcfc0acc272260295c907639a1f85339aa0bb
dampq <Shruthika044@gmail.com>
2026-03-28 13:32:31 +0530

    Delete src directory

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "./app";
-import { config } from "./config";
-
-const app = createApp();
-
-app.listen(config.port, () => {
-  console.log(`Person 3 service running on port ${config.port}`);
-});
